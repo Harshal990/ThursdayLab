627da417c73bb376c1f2708d06b8f3d4c54dbf18
Harshal990 <117341124+Harshal990@users.noreply.github.com>
2022-12-10 17:16:34 +0530

    Add files via upload

---
+++ ShoppingKartProject/ShoppingKart.java
@@ -0,0 +1,14 @@
+package shoppingKartusingarray;
+import java.util.Scanner;
+
+//This is the main entry point
+public class ShoppingKart {
+
+	    static Scanner sc=new Scanner(System.in); 
+	    
+	    
+		public static void main(String[] args) {
+			System.out.println("\t\t~Welcome To Shopping Kart~");
+			ShoppingKartOperations.AllMenus();
+		}
+}
